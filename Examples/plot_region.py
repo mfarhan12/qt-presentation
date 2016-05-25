22a6082e24cdbd6557945f3d659e34d385dcd2dd
Mohammad <mfarhan102@gmail.com>
2016-05-24 21:57:13 -0400

    modified simple plot example

--- Examples/plot_region.py
+++ Examples/plot_region.py
@@ -1,33 +1,46 @@
-from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
+from PySide import QtGui, QtCore
+import sys
+import random
 
-app = QtGui.QApplication([])
+class PlotExample(QtGui.QWidget):
+    def __init__(self):
+        super(PlotExample, self).__init__()
+        self.setWindowTitle("Plot Example")
+        grid = QtGui.QGridLayout()
+        
+        x = np.linspace(-100, 100, 1000)
+        data = np.sin(x) / x
+        
+        full_plot =  pg.PlotWidget(title="Region Selection")
+        full_plot.plot(data, pen=(255,255,255,200))
+        region_selector = pg.LinearRegionItem([400,700])
 
-win = pg.GraphicsWindow(title="Region Selection Example")
-win.resize(1000,600)
-win.setWindowTitle('pyqtgraph example: Plotting')
+        full_plot.addItem(region_selector)
+
+        region_plot = pg.PlotWidget(title="Zoom on selected region")
+        region_plot.plot(data)
+
+        def updatePlot():
+            region_plot.setXRange(*region_selector.getRegion(), padding=0)
+
+        def updateRegion():
+            region_selector.setRegion(region_plot.getViewBox().viewRange()[0])
+
+        region_selector.sigRegionChanged.connect(updatePlot)
+        region_plot.sigXRangeChanged.connect(updateRegion)
+        updatePlot()
+
+        grid.addWidget(full_plot,0,0,1,3)
+        grid.addWidget(region_plot,1,0,1,3)
+
+        self.setLayout(grid)
+        self.show()
 
 
 
-x = np.linspace(-100, 100, 1000)
-data = np.sin(x) / x
-full_plot = win.addPlot(title="Region Selection")
-full_plot.plot(data, pen=(255,255,255,200))
-region_selector = pg.LinearRegionItem([400,700])
-
-full_plot.addItem(region_selector)
-
-win.nextRow()
-
-region_plot = win.addPlot(title="Zoom on selected region")
-region_plot.plot(data)
-def updatePlot():
-    region_plot.setXRange(*region_selector.getRegion(), padding=0)
-def updateRegion():
-    region_selector.setRegion(region_plot.getViewBox().viewRange()[0])
-region_selector.sigRegionChanged.connect(updatePlot)
-region_plot.sigXRangeChanged.connect(updateRegion)
-updatePlot()
-
-QtGui.QApplication.instance().exec_()
+# Launch the application
+app = QtGui.QApplication(sys.argv)
+ex = PlotExample()
+sys.exit(app.exec_())
--- Examples/simple_plot.py
+++ Examples/simple_plot.py
@@ -11,41 +11,18 @@ class PlotExample(QtGui.QWidget):
         grid = QtGui.QGridLayout()
         line_plot = pg.PlotWidget(title="Simple Plot")
         self.curve = line_plot.plot(pen = "r")
-        self.freq_label = QtGui.QLabel("")
-        self.calc_label = QtGui.QLabel("")
-        self.error_label = QtGui.QLabel("")
-        fft_plot = pg.PlotWidget(title="FFT Plot")
-        self.fft_curve = fft_plot.plot(pen = "y")
-        
-        grid.addWidget(self.freq_label,0,0,1,1)
-        grid.addWidget(self.calc_label,0,1,1,1)
-        grid.addWidget(self.error_label,0,2,1,1)
-        
+        line_plot .showGrid(x=True, y=True)
+        line_plot .setLabel('left', "Voltage", units='V')
+        line_plot .setLabel('bottom', "Time", units='T')
+        x = np.linspace(0, 1, 1024)
+        wave = 5 * np.sin(2 * np.pi * 5 * x)
+        self.curve.setData(x, wave)
         grid.addWidget(line_plot,1,0,1,3)
-        grid.addWidget(fft_plot,2,0,1,3)
 
         self.setLayout(grid)
         self.show()
 
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.generate_sine)
-        self.timer.start(500)
 
-    def generate_sine(self):
-        
-        x = np.linspace(0, 1, 1024)
-        freq = random.randint(1,512)
-        self.freq_label.setText("Signal Frequency %0.2f Hz" % freq)
-        wave = 5 * np.sin(2 * np.pi * freq * x)
-        self.curve.setData(wave)
-        wave= wave * np.hanning(len(wave))
-        power_spectrum = np.abs(np.fft.rfft(wave))/len(wave)
-        peak_index =  np.where(power_spectrum == max(power_spectrum))[0][0]
-
-        calc = (float(peak_index) / len(power_spectrum)) * 512
-        self.calc_label.setText("Calculated Frequency: %0.2f Hz" % float(calc))
-        self.error_label.setText("Error: %0.2f %%" % ((calc / float(freq)) * 100))
-        self.fft_curve.setData(power_spectrum)
 
 # Launch the application
 app = QtGui.QApplication(sys.argv)
